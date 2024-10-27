09c069929c455200f1c8fc95bf07ef4efdc743e8
acreatorwithvision <suhas675g@gmail.com>
2024-10-27 11:40:48 +0530

    main_commit

--- script.js
+++ script.js
@@ -2,7 +2,15 @@ let imgBox = document.getElementById("imgBox");
 let qrIMG = document.getElementById("qrIMG");
 let qrtext = document.getElementById("qrtext");
 function generateQR(){
+    if(qrtext.value.length>0){
     qrIMG.src="https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=Example" + qrtext.value;
+    imgBox.classList.add("show-img");
+    }else{
+        qrtext.classList.add('error');
+        setTimeout(()=>{
+            qrtext.classList.remove('error');
+        },1000);
+    }
 
 }
 
